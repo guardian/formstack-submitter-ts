65ca5bf34e74c920aaf6139536c8e0e1763ce22f
Fweddi <freddie.preece@guardian.co.uk>
2021-07-26 11:23:51 +0100

    Bump node and dependencies to v14

--- teamcity.sh
+++ teamcity.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+setupNvm() {
+    export NVM_DIR="$HOME/.nvm"
+    [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+    echo "Installing nvm"
+    nvm install
+    nvm use
+}
+
+setupNvm
+
+echo "Installing yarn"
+npm install -g yarn
+
 echo "Installing Dependencies"
 yarn install
 # Will place .js files in target
