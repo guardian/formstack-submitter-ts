03dda49a2b6d1f8f527f3a126cbecef8a92e3b0e
Rik Roots <rik.roots@guardian.co.uk>
2026-01-28 12:29:45 +0000

    First attempt to fix the AWS callback issue

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,14 +1,25 @@
 import { sendToFormstack } from "./submitter";
 
-export const handler = async (event, context, callback): Promise<void> => {
+// export const handler = async (event, context, callback): Promise<void> => {
+//   console.log("Formstack Submitter ran");
+
+//   try {
+//     const result = await sendToFormstack(event);
+//     console.log("Response from Formstack:", result);
+//     callback(null, result);
+//   } catch (err) {
+//     console.error(err);
+//     callback(err);
+//   }
+// };
+
+export const handler = async (event): Promise<void> => {
   console.log("Formstack Submitter ran");
 
   try {
     const result = await sendToFormstack(event);
     console.log("Response from Formstack:", result);
-    callback(null, result);
   } catch (err) {
     console.error(err);
-    callback(err);
   }
 };
